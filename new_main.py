af463413cb123ee7703345ae9ad3f052234ea4ab
Teodora Petkova <67368977+eliyanovva@users.noreply.github.com>
2021-07-19 11:47:24 +0200

    Add files via upload

--- new_main.py
+++ new_main.py
@@ -5,7 +5,7 @@ Main module of the game.
 
 import json
 import sys
-from state_class_item import Character
+from three_classes import Character
 
 
 def exit_game(json_file):
@@ -16,46 +16,47 @@ def exit_game(json_file):
     json_file.close()
     sys.exit()
 
+if __name__=="__main__":
 
-with open("messages_data.json", "r") as file:
-    data = json.load(file)
+    with open("messages_data.json", "r") as file:
+        data = json.load(file)
 
-INITIAL_LOCATION = 'Elantris'
-Ivan = Character(INITIAL_LOCATION)
+    INITIAL_LOCATION = 'Elantris'
+    Ivan = Character(INITIAL_LOCATION)
 
-while True:
+    while True:
 
-    print('\n Please enter:\
-          \n\'location\' to see the what you can do in your current location',
-          '\n\'items\' to see what you can do with the items in your inventory, \
-          \n\'all\' to see all possible options. \n')
+        print('\nPlease enter:\
+              \n\'location\' to see the what you can do in your current location',
+             '\n\'items\' to see what you can do with the items in your inventory, \
+            \n\'all\' to see all possible options. \n')
 
-    message_input = input()
-    Ivan.print_options(data, message_input)
+        message_input = input()
+        Ivan.print_options(data, message_input)
 
-    print('\nPlease enter the number of the option you have chosen.\n \
-          Additional instructions:\n \
-          \'bag\' to check the current contents of your bag, \n \
-          \'pin\' to check your current location \n \
-          \'q\' to exit the game.')
+        print('\nPlease enter the number of the option you have chosen. \
+               \nAdditional instructions:\n \
+              \'bag\' to check the current contents of your bag, \n \
+              \'pin\' to check your current location \n \
+              \'q\' to exit the game.')
 
-    input_str = input()
+        input_str = input()
 
     #   TODO: invalid input, linting, testing
 
-    if input_str == 'bag':
-        Ivan.print_current_inventory()
-        continue
-    if input_str == 'pin':
-        Ivan.print_current_location()
-        continue
-    if input_str in ('q', '13'):
-        exit_game(file)
+        if input_str == 'bag':
+            Ivan.print_current_inventory()
+            continue
+        if input_str == 'pin':
+            Ivan.print_current_location()
+            continue
+        if input_str in ('q', '13'):
+            exit_game(file)
 
-    if not input_str.isdigit():
-        print('You have entered invalid input, please try again')
-        continue
-    input_int = int(input_str, 10)
+        if not input_str.isdigit():
+            print('You have entered invalid input, please try again')
+            continue
+        input_int = int(input_str, 10)
 
-    message = Ivan.find_message(input_int, data)
-    Ivan.action_on_message(message, data)
+        message = Ivan.find_message(input_int, data)
+        Ivan.action_on_message(message, data)
--- three_classes.py
+++ three_classes.py
@@ -4,156 +4,209 @@ Created on Thu Jul  8 08:29:15 2021
 
 @author: Teodora
 
-TODO: move search message to Character, merge the behaviour of the two functions
-      print options too!
 """
 
 
 class Inventory:
     """
     The class contains a dictionary of items and their quantities
-    
+
     It handles operations like:
         add item
         remove item
-        print contents of the inventory
+        action on an item based on a message from json file
+        get dictionary
     """
     def __init__(self):
         """The contructor initilizes an empty dictionary"""
-        self.__item_dict={}
+        self.__item_dict = {}
 
-    def print_item_options(self, json_file, location):
+    def increment_item(self, item):
         """
-        Prints the messages which are related to the current state of the inventory.
-        Every item's messages are specific for a certain location.
+        Input: Item key
+        If the key exists in the dictionary, it is incremented with 1
+        Otherwise, it is added to the dictionary
         """
-        for i in self.__item_dict:
-            if self.__item_dict.get(i) != 0:
-                for j in json_file[location]['items'][i]:
-                    print(j['string'], '\n')
-
-    def update_items(self, item):
         if item in self.__item_dict:
             self.__item_dict[item] += 1
         else:
             self.__item_dict[item] = 1
 
     def delete_item(self, item):
+        """
+        Input: Item key
+        The value related to the key is decremented with 1
+        """
         self.__item_dict[item] -= 1
 
-    def search_message(self, json_file, input_ID, location):
-        message = {}
-        for item in json_file[location]['items']:
-            for choice in json_file[location]['items'][item]:
-                if choice['ID'] == input_ID:
-                    message = choice
-                    message['cur_item'] = item
-        
-        return message
-
     def message_update_items(self, message, item):
+        """
+        takes action upon a message from the json file
+        currently, decrements with 1 the value of an item for some messages
+        """
         if message['next_item_state'] == 0:
-            self.delete_item(self, item)  
+            self.delete_item(item)
 
     def get_inventory(self):
         """
-        used to show the current state of the inventory to the user
+        returns the dictionary
         """
-        return(self.__item_dict)              
-                    
+        return self.__item_dict
+
 
 class Location:
-    """ 
-    The class has attributes location of type string and item_dict of type dictionary.
-    It describes the location and the current inventory of the game Character.
     """
-    
-    # change input to json file, where location is set to the first location, item_dict is set to the items listed
+    The class contains a location as a string
+
+    It handles operations like:
+        update location
+        get location
+    """
+
     def __init__(self, location):
-        """ 
-        Constructor of the class which initializes the location and the item dictionary. 
+        """
+        Constructor of the class which initializes the location
+        and the item dictionary.
         """
         self.__location = location
 
-    def print_location_options(self, json_file):
-        """
-        Prints the message options which are related to the location of the object
-        """
-        for i in json_file[self.__location]['locationMessages']:
-            print(i['string'], '\n')
-
     def update_location(self, new_location, json_file):
         """
-        Updates the location of the object and prints a welcome message. Should not be used to update to the same location.
+        Updates the location of the object and prints a welcome message.
         """
         if self.__location != new_location:
             print(json_file[new_location]['intro'])
         self.__location = new_location
-        
+
     def get_location(self):
         """
         used to show the current location to the user
         """
-        return self.__location   
-
-    def search_message(self, input, json_file):
-        """
-        input is of type int
-        the function searches for the message with the same ID as the input in the location related messages.
-        Then, the location and the inventory of the object are updated according to the matching message.
-        """
-        message = {}
-        for choice in json_file[self.__location]['locationMessages']:
-            if choice['ID'] == input:
-
-                message = choice
-        return message 
+        return self.__location
 
 
 class Character:
+    """
+    The class performs the action of in the game.
+    It has Location and Inventory attributes and updates them upon
+    different user inputs.
+    """
     def __init__(self, location):
-        """ 
-        Constructor of the class which initializes the location and the item dictionary. 
+        """
+        Constructor of a character class which has a location and an inventory.
+        The class which initializes the location and the item dictionary.
         """
         self.__inventory = Inventory()
         self.__location = Location(location)
-    
+        
     def print_current_location(self):
+        """
+        Prints the current location  of the character.
+        """
         print('You are in ', self.__location.get_location())
-    
+
     def print_current_inventory(self):
-        print('The contents of your bag are: ', self.__inventory.get_inventory())
+        """
+        Prints the inventory of the character.
+        """
+        print('The contents of your bag are: ',
+              self.__inventory.get_inventory())
+
+    def __print_item_options(self, json_file):
+        """
+        Prints the messages related to the current state of the inventory.
+        Every item's messages are specific for a certain location.
+        """
+        for item in self.__inventory.get_inventory():
+            if self.__inventory.get_inventory().get(item) != 0:
+                for j in json_file[self.__location.get_location()]['items'][item]:
+                    print(j['string'], '\n')
+
+    def __print_location_options(self, json_file):
+        """
+        Prints the message options which are related to the location of the object
+        """
+        for message in json_file[self.__location.get_location()]['locationMessages']:
+            print(message['string'], '\n')
 
     def print_options(self, json_file, choice):
+        """
+        Prints all options, per the choice of the user.
+        Asks for new input and calls itself again upon invalid input.
+        """
         if choice == "location":
-            self.__location.print_location_options(json_file)
+            self.__print_location_options(json_file)
         elif choice == "items":
-            self.__inventory.print_item_options(json_file, self.__location.get_location())
+            self.__print_item_options(json_file)
         elif choice == "all":
-            self.__location.print_location_options(json_file)
-            self.__inventory.print_item_options(json_file, self.__location.get_location())
+            self.__print_location_options(json_file)
+            self.__print_item_options(json_file)
         else:
             print('Invalid input, try again')
-            self.print_options(json_file)
+            new_choice = input()
+            self.print_options(json_file, new_choice)
 
-    def find_message(self, input, json_file):
-        message = self.__location.search_message(input, json_file)
-        if message == {}:
-            message = self.__inventory.search_message(json_file, input, self.__location.get_location())
+    def __search_item_message(self, json_file, input):
+        """
+        Searches for a message related to an item which matches the input ID.
+        Returns the message as a dictionary or an empty dictionary if no message
+        matches the input ID.
+        """
+        message = {}
+        json_dict = json_file[self.__location.get_location()]['items']
+        for item in json_dict:
+            for choice in json_dict[item]:
+                if choice['ID'] == input:
+                    message = choice
+                    message['cur_item'] = item
+
         return message
 
-    def action_location_message(self, message, json_file):
+    def __search_location_message(self, input, json_file):
+        """
+        Searches for a message related to a location which matches the input ID.
+        Returns the message as a dictionary or an empty dictionary if no matching
+        ID is found.
+        """
+        message = {}
+        for choice in json_file[self.__location.get_location()]['locationMessages']:
+            if choice['ID'] == input:
+                message = choice
+        return message
+
+    def find_message(self, input, json_file):
+        """
+        Searches for a message which matches the input using
+        the private item message and location message methods defined above.
+        """
+        message = self.__search_location_message(input, json_file)
+        if message == {}:
+            message = self.__search_item_message(json_file, input)
+        return message
+
+    def __action_location_message(self, message, json_file):
+        """
+        Gets a location message dictionary as input and updates the Location and the
+        Inventory of the Character accordingly.
+        """
         self.__location.update_location(message['next_state'], json_file)
         if message['new_item'] != '':
-            self.__inventory.update_items(message['new_item'])
+            self.__inventory.increment_item(message['new_item'])
 
-    def action_item_message(self, message, json_file):
+    def __action_item_message(self, message, json_file):
+        """
+        Gets an item message dictionary as input and updates the Location and
+        the Inventory of the Character accordingly.
+        """
         self.__location.update_location(message['next_state'], json_file)
         if message['next_item_state'] != 1:
             self.__inventory.delete_item(message['cur_item'])
 
     def action_on_message(self, message, json_file):
+        """
+        Updates the Location and Inventory based on a message dictionary input.
+        """
         if 'cur_item' in message.keys():
-            self.action_item_message(message, json_file)
+            self.__action_item_message(message, json_file)
         elif 'new_item' in message.keys():
-            self.action_location_message(message, json_file)
+            self.__action_location_message(message, json_file)
